343b6ed279337f104750e23a2be09c12f998b898
Morgueniadt <N00230651@student.iadt.ie>
2025-02-21 12:00:36 +0000

    fixing barchart definnition

--- john labs/archive/classes/barchart.js
+++ john labs/archive/classes/barchart.js
@@ -1,22 +1,57 @@
 class BarChart {
-    constructor(data, xValue, yValue, chartWidth, chartHeight, barWidth, margin, axisThickness, chartPosX, chartPosY) {
-        this.data = data;
-        this.xValue = xValue;
-        this.yValue = yValue;
-        this.chartWidth = chartWidth;
-        this.chartHeight = chartHeight;
-        this.barWidth = barWidth;
-        this.margin = margin;
-        this.axisThickness = axisThickness;
-        this.chartPosX = chartPosX;
-        this.chartPosY = chartPosY;
-        this.maxValue = max(this.data.map(row => row[this.yValue]));
-        this.scaler = this.chartHeight / this.maxValue;
+    constructor(obj) {
+        this.data = obj.data;
+        this.xValue = obj.xValue;
+        this.yValue = obj.yValue;
+        this.chartHeight = obj.chartHeight || 300;
+        this.chartWidth = obj.chartWidth || 300;
+        this.barWidth = obj.barWidth || 10;
+        this.margin = obj.margin || 10;
+        this.axisThickness = obj.axisThickness || 1;
+        this.axisTickThickness = obj.axisTickThickness || 1;
+
+        this.chartPosX = obj.xPos || 50;
+        this.chartPosY = obj.yPos || 350;
+
+        // Calculate gap between bars dynamically
         this.gap = (this.chartWidth - (this.data.length * this.barWidth) - (this.margin * 2)) / (this.data.length - 1);
 
-        this.axisColour = color(255, 204, 0);
-        this.barColour = color(0, 200, 50);
-        this.axisTextColour = color(125);
+        // Calculate max value from CSV data dynamically
+        this.maxValue = Math.max(...this.data.map(row => parseFloat(row[this.yValue]) || 0));
+
+        // Calculate scaler
+        this.scaler = this.chartHeight / this.maxValue;
+
+        // Colors
+        this.axisColour = color(169, 169, 169);  // Light Gray (Axis)
+        this.axisTickColour = color(176, 224, 230);  // Light Blue (Ticks)
+        this.barColour = color(168, 230, 207);  // Pastel Green (Bars)
+        this.axisTextColour = color(0, 0, 0);
+
+        this.numTicks = 10;
+        this.tickLength = 10;
+    }
+
+    renderBars() {
+        push();
+        translate(this.chartPosX, this.chartPosY);
+
+        push();
+        translate(this.margin, 0);
+        for (let i = 0; i < this.data.length; i++) {
+            let xPos = (this.barWidth + this.gap) * i;
+            fill(this.barColour);
+            noStroke();
+            rect(xPos, 0, this.barWidth, -this.data[i][this.yValue] * this.scaler);
+
+            push();
+            fill(this.axisTextColour);
+            textAlign(LEFT, CENTER);
+            translate(xPos + (this.barWidth / 2), 15);
+            pop();
+        }
+        pop();
+        pop();
     }
 
     renderAxis() {
@@ -25,44 +60,71 @@ class BarChart {
         noFill();
         stroke(this.axisColour);
         strokeWeight(this.axisThickness);
-        line(0, 0, 0, -this.chartHeight); // Y Axis
-        line(0, 0, this.chartWidth, 0); // X Axis
+        line(0, 0, 0, -this.chartHeight);
+        line(0, 0, this.chartWidth, 0);
         pop();
     }
 
     renderTicks() {
         push();
         translate(this.chartPosX, this.chartPosY);
-        fill(this.axisTextColour);
-        noStroke();
-        textAlign(CENTER, CENTER);
-        textSize(10);
+        noFill();
 
-        // Y Axis Ticks (optional, based on your need)
-        for (let i = 0; i <= 5; i++) {
-            let tickValue = map(i, 0, 5, 0, this.maxValue);
-            text(tickValue, -10, -map(i, 0, 5, 0, this.chartHeight));
+        // Set stroke for tick marks
+        stroke(this.axisTickColour);
+        strokeWeight(this.axisTickThickness);
+
+        let tickIncrement = this.chartHeight / this.numTicks;
+        let valueIncrement = this.maxValue / this.numTicks;
+
+        for (let i = 0; i <= this.numTicks; i++) {
+            let y = -tickIncrement * i;
+            let value = (i * valueIncrement).toFixed(0); // Round values
+
+            // Draw tick mark
+            stroke(this.axisTickColour);
+            strokeWeight(this.axisTickThickness);
+            line(0, y, -this.tickLength, y);
+
+            // Draw numerical indicator
+            noStroke();
+            fill(0);
+            textAlign(RIGHT, CENTER);
+            text(value, -this.tickLength - 5, y);
         }
 
-        // X Axis Ticks
-        for (let i = 0; i < this.data.length; i++) {
-            let xPos = (this.barWidth + this.gap) * i + this.margin;
-            text(this.data[i][this.xValue], xPos, 10); // Track name
-        }
         pop();
     }
 
-    renderBars() {
+    renderLabels() {
         push();
         translate(this.chartPosX, this.chartPosY);
-        for (let i = 0; i < this.data.length; i++) {
-            let xPos = (this.barWidth + this.gap) * i + this.margin;
-            let barHeight = -this.data[i][this.yValue] * this.scaler;
 
-            fill(this.barColour);
-            noStroke();
-            rect(xPos, 0, this.barWidth, barHeight);
+        push();
+        translate(this.margin, 0);
+        for (let i = 0; i < this.data.length; i++) {
+            let xPos = (this.barWidth + this.gap) * i;
+
+            push();
+            fill(this.axisTextColour);
+            textAlign(LEFT, CENTER);
+            translate(xPos + (this.barWidth / 2), 15);
+            textSize(15);
+            rotate(60);
+            text(this.data[i][this.xValue], 0, 0);
+            pop();
         }
+        pop();
+        pop();
+    }
+
+    renderTitle() {
+        push();
+        translate(this.chartPosX + this.chartWidth / 2, this.chartPosY - this.chartHeight - 20);
+        fill(this.axisTextColour);
+        textAlign(CENTER, CENTER);
+        textSize(18);
+        text("Chart Title: Most Streamed Spotify Songs", 0, 0);
         pop();
     }
 }
